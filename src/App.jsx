ced9385d8e1deb9724b1342111c13324d13835bf
Kaori <studiokaori.com>
2020-11-04 15:30:37 +0100

    Create TrackHome

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import TrackHome from './components/TrackHome';
 
 export default function App() {
   return (
-    <div>
-      <h1>react</h1>
-    </div>
+    <Router>
+      <div>
+        <h1>react</h1>
+        <Switch>
+          <Route path="/" exact component={TrackHome} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
---
+++ src/components/TrackHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TrackHome() {
+  return (
+    <div>
+      <h1>Track home</h1>
+    </div>
+  );
+}
