0a2e592aeb13797ca24381992faae56ea852f4c9
Kaori <studiokaori.com>
2020-11-05 22:55:05 +0100

    Add recoil to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+// for multi-lang support
+import { userTranslation, useTranslation } from "react-i18next";
+
+//recoil library
+import { atom, useRecoilState, useRecoilValue } from "recoil";
+import { langState } from "../js/state-information";
+
+import Header from "./parts/Header";
 
 export default function Login() {
+  // for multi-lang support
+  const [t, i18n] = useTranslation();
+  const [lang, setLang] = useRecoilState(langState);
+
+  useEffect(() => {
+    console.log(lang);
+    i18n.changeLanguage(lang);
+  }, [lang, i18n]);
+
   return (
     <div>
+      <Header />
       <h1>Track home</h1>
+      <h1>{t("Tracking result")}</h1>
     </div>
   );
 }
