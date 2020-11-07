554c5d98bc1ae4f358eed5a94263e6fe67a42f51
Kaori <studiokaori.com>
2020-11-07 21:25:30 +0100

    Add sort

--- src/components/RealTrackingResults.jsx
+++ src/components/RealTrackingResults.jsx
@@ -11,68 +11,56 @@ import { useLocation } from "react-router-dom";
 
 import Card from "./parts/Card";
 import Header from "./parts/Header";
-// todo replace fake db to real
-import Data from "../PackageTrackingData.json";
 
 // import css
 import "../css/trackingResults.css";
 
-export default function TrackingResults({ match }) {
+export default function RealTrackingResults({ match }) {
   console.log("TrackingResults");
 
   // for multi-lang support
   const [t, i18n] = useTranslation();
   const [lang, setLang] = useRecoilState(langState);
 
-  // todo set0 when fake db is replaced
   // loading status, 0= loading, 1=ready, 2=loading error
-  const [status, setStatus] = useState(1);
+  const [status, setStatus] = useState(0);
 
   // user name and phone
   const location = useLocation();
-  console.log("location :", location);
-
   const userName = location.state.userName;
 
-  // todo remove param 'data' from useState later, useState(), param should be empty
-  const [information, setInformation] = useState(Data);
-  //const endpoint = "https://my.api.mockaroo.com/orders.json?key=e49e6840";
+  //data
+  const endpoint = "https://my.api.mockaroo.com/orders.json?key=e49e6840";
 
-  // todo change to useState([]), after replace fake DB
-  //inside of useeffect, setcard.
-  const [cards, setCards] = useState(
-    information
-      .filter(
-        (parcel) =>
-          removeSpaceAndlower(parcel.user_name) ===
-          removeSpaceAndlower(userName)
-      )
-      .map((parcel) => <Card key={parcel.id} parcelData={parcel} />)
-  );
+  const [cards, setCards] = useState([]);
 
   // todo replace fake db to real
-  /*
   useEffect(() => {
     const getData = async () => {
       try {
         // Fetch whole data
-        const response = await fetch(endpoint, { mode: 'cors' });
+        const response = await fetch(endpoint, { mode: "cors" });
         const data = await response.json();
 
-        setInformation(data);
-        console.log('fetch data, ok');
-        console.log('data :', data);
+        setCards(
+          data
+            .filter(
+              (parcel) =>
+                removeSpaceAndlower(parcel.user_name) ===
+                removeSpaceAndlower(userName)
+            )
+            .map((parcel) => <Card key={parcel.id} parcelData={parcel} />)
+        );
+
+        setStatus(1);
       } catch {
         // Set status data loading error(2)
         setStatus(2);
-        console.log('fetch data, no');
       }
     };
 
     getData();
   }, []);
-
-  */
 
   // for multi-lang support
   useEffect(() => {
@@ -102,20 +90,20 @@ export default function TrackingResults({ match }) {
 
         <section className="content">
           <div className="content-wrapper">
-            {/* cards */}
-            {/* if status is ready(1), display multiple cards.
-      Otherwise display error.       */}
-
-            {status === 0 ? <p>Loading...</p> : null}
+            {status === 0 ? (
+              <div className="display-status">Loading...</div>
+            ) : null}
             {status === 1 ? (
               cards.length === 0 ? (
-                <div className="no-result">{t("No result")}</div>
+                <div className="display-status">{t("No result")}</div>
               ) : (
                 cards
               )
             ) : null}
             {status === 2 ? (
-              <p>Data loading error. Please try again later.</p>
+              <div className="display-status">
+                Data loading error. Please try again later.
+              </div>
             ) : null}
           </div>
         </section>
--- src/components/TrackingResults.jsx
+++ src/components/TrackingResults.jsx
@@ -11,6 +11,8 @@ import { useLocation } from "react-router-dom";
 
 import Card from "./parts/Card";
 import Header from "./parts/Header";
+// todo replace fake db to real
+import Data from "../PackageTrackingData.json";
 
 // import css
 import "../css/trackingResults.css";
@@ -24,46 +26,49 @@ export default function TrackingResults({ match }) {
 
   // todo set0 when fake db is replaced
   // loading status, 0= loading, 1=ready, 2=loading error
-  const [status, setStatus] = useState(0);
+  const [status, setStatus] = useState(1);
 
   // user name and phone
   const location = useLocation();
+  console.log("location :", location);
+
   const userName = location.state.userName;
 
-  //data
-  const endpoint = "https://my.api.mockaroo.com/orders.json?key=e49e6840";
+  // todo remove param 'data' from useState later, useState(), param should be empty
+  const [information, setInformation] = useState(Data);
+  //const endpoint = "https://my.api.mockaroo.com/orders.json?key=e49e6840";
 
   // todo change to useState([]), after replace fake DB
   //inside of useeffect, setcard.
-  const [cards, setCards] = useState([]);
+  const defaltData = information.filter(
+    (parcel) =>
+      removeSpaceAndlower(parcel.user_name) === removeSpaceAndlower(userName)
+  );
+  const [cards, setCards] = useState(sortAndCreatCards("last_updated"));
 
   // todo replace fake db to real
+  /*
   useEffect(() => {
     const getData = async () => {
       try {
         // Fetch whole data
-        const response = await fetch(endpoint, { mode: "cors" });
+        const response = await fetch(endpoint, { mode: 'cors' });
         const data = await response.json();
 
-        setCards(
-          data
-            .filter(
-              (parcel) =>
-                removeSpaceAndlower(parcel.user_name) ===
-                removeSpaceAndlower(userName)
-            )
-            .map((parcel) => <Card key={parcel.id} parcelData={parcel} />)
-        );
-
-        setStatus(1);
+        setInformation(data);
+        console.log('fetch data, ok');
+        console.log('data :', data);
       } catch {
         // Set status data loading error(2)
         setStatus(2);
+        console.log('fetch data, no');
       }
     };
 
     getData();
   }, []);
+
+  */
 
   // for multi-lang support
   useEffect(() => {
@@ -74,6 +79,27 @@ export default function TrackingResults({ match }) {
   // remove space and lower case for string
   function removeSpaceAndlower(str) {
     return str.replace(/\s+/g, "").toLowerCase();
+  }
+
+  // for sort
+  var sorts = [
+    { code: "last_updated", name: t("Last update") },
+    { code: "eta", name: t("ETA") },
+    { code: "Sender", name: t("Sender") },
+  ];
+
+  var sortOptions = sorts.map((sort) => (
+    <option key={sort.code} value={sort.code}>
+      {sort.name}
+    </option>
+  ));
+  function sortAndCreatCards(key) {
+    const sortedResults = information.sort((a, b) =>
+      a[key] > b[key] ? 1 : -1
+    );
+    return sortedResults.map((parcel) => (
+      <Card key={parcel.id} parcelData={parcel} />
+    ));
   }
 
   return (
@@ -92,21 +118,32 @@ export default function TrackingResults({ match }) {
         </header>
 
         <section className="content">
+          <div className="sort">
+            <i class="fas fa-sort"></i>
+            <select
+              onChange={(event) =>
+                setCards(sortAndCreatCards(event.target.value))
+              }
+            >
+              {sortOptions}
+            </select>
+          </div>
+
           <div className="content-wrapper">
-            {status === 0 ? (
-              <div className="display-status">Loading...</div>
-            ) : null}
+            {/* cards */}
+            {/* if status is ready(1), display multiple cards.
+      Otherwise display error.       */}
+
+            {status === 0 ? <p>Loading...</p> : null}
             {status === 1 ? (
               cards.length === 0 ? (
-                <div className="display-status">{t("No result")}</div>
+                <div className="no-result">{t("No result")}</div>
               ) : (
                 cards
               )
             ) : null}
             {status === 2 ? (
-              <div className="display-status">
-                Data loading error. Please try again later.
-              </div>
+              <p>Data loading error. Please try again later.</p>
             ) : null}
           </div>
         </section>
